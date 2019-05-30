0afecc3aa8aaf120e7a452b60b4673fa252c3fb5
Jonathan <jonathan.zalazar@aossas.com>
2019-05-29 19:14:53 -0500

    Creación de peticion POST

--- handler.js
+++ handler.js
@@ -8,7 +8,15 @@ module.exports.hello = async (event) => {
             input: event.name,
         }, null, 2),
     };
+};
 
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
+module.exports.showUser = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Peticion POST`,
+            input: event['body'],
+        }, null, 2),
+    };
 };
